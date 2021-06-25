5355f97c8d6d29e635e7ddbbac5003dd0f725876
fauster <lucasfauster@id.uff.br>
2021-06-25 06:20:21 -0300

    refatorando ex4

--- src/main/sql/s20211/exercicios/exercicio4/LucasFauster.sql
+++ src/main/sql/s20211/exercicios/exercicio4/LucasFauster.sql
@@ -1,17 +1,23 @@
 CREATE FUNCTION removeLinhaColuna(i int, j int, mat float[][]) RETURNS float[][] AS $$
 DECLARE
     resp float[][];
+    currentLin int;
+    currentCol int;
 BEGIN
     matlin := array_ndims(mat);
     matcol :=  array_ndims(mat[0]);
 
+    currentLin := 0;
     FOR linha IN 0..matlin LOOP
         IF linha <> i THEN
-            Resp := array_append(resp, ARRAY[]);
+            resp := array_append(resp, ARRAY[]);
+            currentLin += 1;
         END IF;
+        currentCol := 0;
         FOR coluna IN 0..matcol LOOP
             IF linha <> i AND coluna <> j THEN
-                resp[linha] := array_append(resp[linha], 0);
+                resp[currentLin][currentCol] := array_append(resp[currentLin][currentCol], mat[linha][coluna]);
+                currentCol += 1;
             END IF;
         END LOOP;
     END LOOP;
